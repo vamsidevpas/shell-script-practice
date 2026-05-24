6dcf6427d9d0d6da53ffdc67b48e996da4ada9fc
vamsi <vamsikrishnaydv@gmail.com>
2026-05-24 12:49:26 +0530

    Add variables_lat.sh script

---
+++ variables_lat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "please enter your username ::"
+read USER_NAME #here USER_NAME is a variable, whatever you have enter in the terminal stores in this variable
+
+echo"username is USER_NAME
