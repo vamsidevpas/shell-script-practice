2e3b39d6499be180ef62f81bfbd3a9b74044fa4e
vamsi <vamsikrishnaydv@gmail.com>
2026-05-24 12:38:18 +0530

    Add variables.sh script

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1: Today $person2, will go and buy gold today"
+echo "$person2: lets $person1 ,go and check "
+echo "$person1: Do $person2 check just buy"
+echo "$person2: $person1 ,okay"
