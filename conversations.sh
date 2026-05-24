3a4d12338c62b887b6fa3f32a2ee97a3b0c4f943
vamsi <vamsikrishnaydv@gmail.com>
2026-05-24 12:24:32 +0530

    Add conversations.sh script

--- conversations.sh
+++ conversations.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+person1=wife
+person2=Husband
 
-echo "wife: Today will go and buy gold today"
-echo "Husband: lets go and check "
-echo "wife: Do check just buy"
-echo "Husband: okay"
+echo "$person1: Today will go and buy gold today"
+echo "$person2: lets go and check "
+echo "$person1: Do check just buy"
+echo "$person2: okay"
