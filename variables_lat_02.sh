53efea55b88c0fc8efcfe73d27231966f88dd730
vamsi <vamsikrishnaydv@gmail.com>
2026-05-25 04:01:59 +0530

    shell script started

--- variables_lat_02.sh
+++ variables_lat_02.sh
@@ -6,5 +6,6 @@ echo "script name:$0"
 echo "who is running this : $USER"
 echo "which director :$PWD"
 sleep 5 &
+echo "PID of the background command running just now: $!"
 wait $ !
 echo "script executed in $seconds seconds"
